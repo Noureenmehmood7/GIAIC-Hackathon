262540d885596cfe44c45eb0877e5dbd3d4bb51a
Noureenmehmood7 <noureenmehmood7@gmail.com>
2024-12-09 23:09:29 +0500

    fixed

--- src/app/ourChefs/page.tsx
+++ src/app/ourChefs/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Hero from "./Hero"
 import ChefGrid from "./ChefGrid"
-import chef1 from "../assets/ChefCard1.png";
-import chef2 from "../assets/Card2.png";
-import chef3 from "../assets/Card3.png";
-import chef4 from "../assets/Card4.png";
+// import chef1 from "../assets/ChefCard1.png";
+// import chef2 from "../assets/Card2.png";
+// import chef3 from "../assets/Card3.png";
+// import chef4 from "../assets/Card4.png";
 import Image from "next/image"
 
 const OurChefs = () => {
@@ -24,10 +24,10 @@ const OurChefs = () => {
         </h1>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center gap-[20px] mt-[50px]">
-          <Image src={chef1} alt="" className="w-[200px] md:w-[305px] md:h-[280px] cursor-pointer"/>
+          {/* <Image src={chef1} alt="" className="w-[200px] md:w-[305px] md:h-[280px] cursor-pointer"/>
           <Image src={chef2} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
           <Image src={chef3} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
-          <Image src={chef4} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" />
+          <Image src={chef4} alt="" className="w-[200px] md:w-[280px] md:h-[280px] cursor-pointer" /> */}
         </div>
       </section>
     </div>
